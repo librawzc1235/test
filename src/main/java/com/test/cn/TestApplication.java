8e9f78440a139f02236d7b8da303a63ecb0b23b2
YQD-190211 <YQD-190211@windows10.microdone.cn>
2019-10-30 10:39:13 +0800

    配置数据源

--- src/main/java/com/test/cn/TestApplication.java
+++ src/main/java/com/test/cn/TestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})//这里的配置文件是没有配置数据库的
+@SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
