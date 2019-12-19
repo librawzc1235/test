98abb29a1d2a99f3d3a466e259043e6ccad566a5
YQD-190211 <YQD-190211@windows10.microdone.cn>
2019-12-19 09:36:52 +0800

    测试前台接收的json数组对象

---
+++ src/main/java/com/test/cn/entity/JsonObjects.java
@@ -0,0 +1,15 @@
+package com.test.cn.entity;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class JsonObjects {
+
+	public String id;
+	
+	public String name;
+	
+	List<String> stringList;
+}
